6407a25444242dae1e3290b5ecf1972b2a2e1273
wezmiolem <ch.maciek@gmail.com>
2021-09-10 17:21:48 +0200

    position struct created

---
+++ Utils/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechAssessment.Utils
+{
+    public struct Position
+    {
+        public int X { get; }
+
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
