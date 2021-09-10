f30343c0ac4c918660c6aa61f7067af4eb085369
wezmiolem <ch.maciek@gmail.com>
2021-09-10 18:16:11 +0200

    direction enum created

---
+++ Utils/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechAssessment.Utils
+{
+    public enum  Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+}
