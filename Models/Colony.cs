59dd42a3c810b5ac9416f1dcafa59dc5c2b53226
wezmiolem <ch.maciek@gmail.com>
2021-09-10 17:16:12 +0200

    colony model added

---
+++ Models/Colony.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechAssessment.Models
+{
+    public class Colony
+    {
+        public int Width { get; }
+
+        public BaseAnt[,] _ants;
+    }
+}
