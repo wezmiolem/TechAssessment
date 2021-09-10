889bab4aad4749be77804d80b509a848e7d7f9f3
wezmiolem <ch.maciek@gmail.com>
2021-09-10 17:32:25 +0200

    added constructors for ants

--- Models/BaseAnt.cs
+++ Models/BaseAnt.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TechAssessment.Utils;
 
 namespace TechAssessment.Models
 {
     public abstract class BaseAnt
     {
+        public Position Coords { get; set; }
 
+        protected Colony Colony { get; }
+
+        protected BaseAnt(Colony colony, Position coords)
+        {
+            Colony = colony;
+            Coords = coords;
+        }
     }
 }
